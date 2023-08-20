960104ff3d627e9af62fe7d38f6fa3d52fb63fec
Niteshgorai <niteshkumar7004717@gmail.com>
2023-08-21 00:03:24 +0530

    1st commiit

---
+++ ProductManagement/src/test/java/net/codejava/ProductManagement/PasswordGenerator.java
@@ -0,0 +1,14 @@
+package net.codejava.ProductManagement;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String rawPassword = "tommy";
+		String encodedPassword = encoder.encode(rawPassword);
+		System.out.println(encodedPassword);
+	}
+
+}
